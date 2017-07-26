b86d3fb520058c13ded80fae372298e150afd15d
wzjaixxy <294673951@qq.com>
2017-07-26 17:26:22 +0800

    直流

---
+++ 直流电机/RTE/RTE_Components.h
@@ -0,0 +1,16 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'tt' 
+ * Target:  '直流电机' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_RCC
+
+#endif /* RTE_COMPONENTS_H */
