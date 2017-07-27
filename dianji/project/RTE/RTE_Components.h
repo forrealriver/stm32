8e859f682a08b998652f2733e9b3fc953657c9b7
wzjaixxy <294673951@qq.com>
2017-07-27 17:52:30 +0800

    1

--- dianji/project/RTE/RTE_Components.h
+++ dianji/project/RTE/RTE_Components.h
@@ -3,14 +3,12 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'tt' 
- * Target:  '直流电机' 
+ * Project: 'dianji' 
+ * Target:  'Target 1' 
  */
 
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
-#define RTE_DEVICE_STDPERIPH_FRAMEWORK
-#define RTE_DEVICE_STDPERIPH_RCC
 
 #endif /* RTE_COMPONENTS_H */
---
+++ dianji/project/system_stm32f10x.d
@@ -0,0 +1,8 @@
+.\system_stm32f10x.o: RTE\Device\STM32F103C8\system_stm32f10x.c
+.\system_stm32f10x.o: E:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.1.0\Device\Include\stm32f10x.h
+.\system_stm32f10x.o: F:\GitHub\STM32_study\STM32_study\dianji\project\RTE\RTE_Components.h
+.\system_stm32f10x.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cm3.h
+.\system_stm32f10x.o: E:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\system_stm32f10x.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cmInstr.h
+.\system_stm32f10x.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cmFunc.h
+.\system_stm32f10x.o: E:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.1.0\Device\Include\system_stm32f10x.h
